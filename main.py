cd814617b920ea8003595d77847a11c3b7310136
castillo <miguelcastillo207@gmail.com>
2023-09-18 15:21:16 -0500

    Initial clean commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 00:44:25 2021
+
+@author: Miguel Castillo
+"""
+import cv2
+import numpy as np
+import os
+import csv
+import tensorflow as tf
+import handshape_feature_extractor as hfe
+
+
+# import the handfeature extractor class
+from handshape_feature_extractor import HandShapeFeatureExtractor
+
+# =============================================================================
+# Get the penultimate layer for trainig data
+# =============================================================================
+# your code goes here
+# Extract the middle frame of each gesture video
+
+
+# =============================================================================
+# Get the penultimate layer for test data
+# =============================================================================
+# your code goes here
+# Extract the middle frame of each gesture video
+
+
+# =============================================================================
+# Recognize the gesture (use cosine similarity for comparing the vectors)
+# =============================================================================
